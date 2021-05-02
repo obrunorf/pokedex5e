35e9f449bee23e36889f2d5f0db8d80f96f2fb7e
obrunorf <obrunorf@gmail.com>
2021-05-02 17:03:28 -0400

    Initial commit

---
+++ src/components/ListaPokemons.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <li v-for="(name, number) in pokemonsObj" v-bind:key="number">
+      {{ number }} - {{ name }}</li>
+  </div>
+</template>
+
+<script>
+import pokemons from "../data/index_order.json";
+export default {
+  data() {
+    return {
+      pokemons: MY_JSON,
+    };
+  },
+};
+console.log(pokemons);
+
+let pokemonsObj = [{}];
+
+for (let i = 0; i < Object.keys(pokemons).length; i++) {
+  if (pokemons[i] !== undefined) {
+    var p = {
+      number: i + 1,
+      name: pokemons[i][0],
+    };
+    pokemonsObj.push(p);
+  }
+}
+console.log(pokemonsObj);
+</script>
+
+<script setup>
+import { defineProps, reactive } from "vue";
+</script>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,7 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+import "./index.css";
+
+createApp(App).mount("#app");
+//console.log(pokemons);
+// pokemonIndex.forEach(x => { console.log(x.); });
