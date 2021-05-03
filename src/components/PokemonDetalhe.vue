917707f3c8cfc71eba5b1b183a98ca061a00271b
obrunorf <obrunorf@gmail.com>
2021-05-02 22:48:57 -0400

    amem

--- src/components/PokemonDetalhe.vue
+++ src/components/PokemonDetalhe.vue
@@ -4,16 +4,27 @@
   </div>
   <div v-else>
     <h1>{{ pokemon.name }}</h1>
+
+    <div>
+      <img       
+        class="h-48" 
+        :src="
+          'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/' +
+          pokemon.index +
+          '.png'
+        "
+      />
+    </div>
   </div>
 </template>
 
 <script setup>
 import PokemonTipo from "./PokemonTipo.vue";
 
-import { defineProps } from 'vue'
+import { defineProps } from "vue";
 defineProps({
-  pokemon: Object
-})
+  pokemon: Object,
+});
 </script>
 
 <style scoped>
